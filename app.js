467ee0b8eed39c63194549e8ac3eebb45a30fa14
Lenovo@DESKTOP-1A9ET6S <haikalfebriansyah743@gmail.com>
2025-04-27 12:48:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const API_URL = 'http://localhost:3000/api/products'; // Ganti dengan IP EC2 backend kamu
+
+async function fetchProducts() {
+  try {
+    const response = await fetch(API_URL);
+    const products = await response.json();
+    const container = document.getElementById('product-container');
+
+    products.forEach(product => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      card.innerHTML = `
+        <img src="${product.image_url}" alt="${product.name}">
+        <div class="card-body">
+          <h2 class="card-title">${product.name}</h2>
+          <p class="card-price">Rp ${parseFloat(product.price).toLocaleString('id-ID')}</p>
+        </div>
+      `;
+
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
+
+fetchProducts();
